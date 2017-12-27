5ed92f8b1813da5a9e04dea55464b1557b652d5a
Ivan Petrov <ivan.petrov@mocoding.com>
2017-12-27 21:25:12 +0200

    Fixed AddOdataApiEasyDocDb

--- src/Mocoding.AspNetCore.ODataApi.EasyDocDb/Extensions.cs
+++ src/Mocoding.AspNetCore.ODataApi.EasyDocDb/Extensions.cs
@@ -1,10 +1,17 @@
-﻿namespace Mocoding.AspNetCore.ODataApi.EasyDocDb
+﻿using Mocoding.EasyDocDb;
+
+namespace Mocoding.AspNetCore.ODataApi.EasyDocDb
 {
     public static class Extensions
     {
         public static IODataApiBuilder AddOdataApiEasyDocDb(this IODataApiBuilder oDataApiBuilder, string foder = "")
         {
             return oDataApiBuilder.UseFactory(new Factory(foder == string.Empty ? "../data" : "../data" + "/" + foder));
+        }
+
+        public static IODataApiBuilder AddOdataApiEasyDocDb(this IODataApiBuilder oDataApiBuilder, string connection, IDocumentStorage storage)
+        {
+            return oDataApiBuilder.UseFactory(new Factory(connection, storage));
         }
     }
 }
