545cdf51885db0114d8b045da256e03fdee1f7df
Dennis Miasoutov <dennis.miasoutov@mocoding.com>
2018-04-19 15:50:31 -0400

    Updated to enable standard uuid.

--- src/Mocoding.AspNetCore.ODataApi.MongoDb/Extensions.cs
+++ src/Mocoding.AspNetCore.ODataApi.MongoDb/Extensions.cs
@@ -1,9 +1,12 @@
-﻿namespace Mocoding.AspNetCore.ODataApi.MongoDb
+﻿using MongoDB.Driver;
+
+namespace Mocoding.AspNetCore.ODataApi.MongoDb
 {
     public static class Extensions
     {
         public static IODataApiBuilder AddODataApiMongoDb(this IODataApiBuilder oDataApiBuilder, string connection)
         {
+            MongoDefaults.GuidRepresentation = MongoDB.Bson.GuidRepresentation.Standard;
             return oDataApiBuilder.UseFactory(new MongoDbFactory(connection));
         }
     }
